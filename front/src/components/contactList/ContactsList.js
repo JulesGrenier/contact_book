6b9b5078e5ca1a1687831f11bd8cdcb88dadaed7
Jules Grenier <grenier.jules@outlook.com>
2018-12-03 16:11:13 +0100

    contact list overflow update

--- front/src/components/contactList/ContactsList.js
+++ front/src/components/contactList/ContactsList.js
@@ -8,22 +8,24 @@ const ContactsList = ({ contacts }) => {
 
       <Header />
 
-      {
-        contacts &&
-        contacts.map(contact => (
-          <div
-            key={contact.id}
-            className="contact"
-          >
-            <img
-            src="https://avatars3.githubusercontent.com/u/34350330?s=400&u=db259e4a5dca73ed6ea5a941e0b6fc41b42b62b5&v=4"
-            alt={contact.firstname}
-            className="contact-avatar"
-            />
-            <h3 className="contact-name">{contact.firstname} {contact.lastname}</h3>
-          </div>
-        ))
-      }
+      <div className="contacts">
+        {
+          contacts &&
+          contacts.map(contact => (
+            <div
+              key={contact.id}
+              className="contact"
+            >
+              <img
+              src="https://avatars3.githubusercontent.com/u/34350330?s=400&u=db259e4a5dca73ed6ea5a941e0b6fc41b42b62b5&v=4"
+              alt={contact.firstname}
+              className="contact-avatar"
+              />
+              <h3 className="contact-name">{contact.firstname} {contact.lastname}</h3>
+            </div>
+          ))
+        }
+      </div>
 
     </div>
   );
